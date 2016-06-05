6a4f65bf569bc7c1aefe1233feea026f735caeee
Kruglov Dmitry <gryphon-dk@mail.ru>
2016-06-05 14:33:25 +0300

    Add tree

--- Labulya/Labulya/tree.h
+++ Labulya/Labulya/tree.h
@@ -1,151 +1,203 @@
 #pragma once
-
-class TValue
-{
-public:
-    int  type()         { return m_type; }
-    void setType(int t) { m_type=t; }
-
-    int  value()         { return m_value; }
-    void setValue(int v) { m_value=v; }
-
-    Tvalue operator + (Tvalue o1, Tvalue o2);
-    bool   operator = (Tvalue o1, Tvalue o2);
-    void   print();
-
-private:
-    int    m_type;
-    double m_value;
-};
+#include "lec.h"
+#include "heap.h"
 
 class TNode
 {
-public:
-   TNode()  {};
-   ~TNode() {};
+	public:
+		TNode()  {};
+		~TNode() {};
 
-   virtual TValue exec();
-   virtual void print();
+		virtual lexeme* exec();
+		virtual void print();
 };
 
-class TConst : public TNode
+class TConst : TNode
 {
-public:
-   TConst();
-   ~TConst();
+	public:
+		TConst(lexeme* mconst)
+		{
+			m_const = mconst;
+		}
+		~TConst() {}
 
-   TValue exec() { return m_const; };
-   void print(); //  { printf("%g", m_const.value()); };
-
-protected:
-   TValue m_const;
+		lexeme* exec()
+		{
+			return m_const;
+		}
+		void print()
+		{
+			printf("%s", m_const->Data);
+		}
+	protected:
+		lexeme* m_const;
 };   
 
-class TVariable : public TNode
+class TVariable : TNode
 {
+	public:
+		TVariable(lexeme* mvariable)
+		{
+			m_variable = m_variable;
+		}
+		~TVariable() {}
+			
+		lexeme* exec()
+		{
+			return m_variable;
+		}
+		void print()
+		{
+			printf("%s", m_variable->Name);
+		}
+	protected:
+		lexeme* m_variable;
 };
 
-class TUnaryOperation : public TNode
+class TUnaryOperation : TNode
 {
-public:
-    TUnaryOperation();
-    ~TUnaryOperation();
-
-   TValue exec(); // { Tvalue res= m_operand->exec();
-                  //   res.setValue(-res.value());
-                  //   return res; };
-   void print();  // { printf("-"); m_operand->print(); }
-protected:
-   int     m_type;
-   TNode   m_operand;
+	public:
+		TUnaryOperation(TNode* operand)
+		{
+			m_operand = operand;
+		}
+		~TUnaryOperation();
+			
+		lexeme exec()
+		{ 
+			lexeme res= m_operand->exec();
+            res.DataChange("-" + res.Data);
+            return res;
+		}
+		void print()
+		{
+			printf("-");
+			m_operand->print();
+		}
+	protected:
+		int    m_type;
+		TNode* m_operand;
 };
 
-class TBinaryOperation : public TNode
+class TBinaryOperation : TNode
 {
-public:
-    TBinaryOperation();
-    ~TBinaryOperation();
+	//бинарный "+"
+	public:
+		TBinaryOperation(TNode*  operand1, TNode*  operand2)
+		{
+			m_operand1 = operand1;
+			m_operand2 = operand2;
+		}
+		~TBinaryOperation();
 
-   TValue exec(); // { Tvalue res1= m_operand1->exec();
-                  //   Tvalue res2= m_operand2->exec();
-                  //   Tvalue res;
-                  //   res.setValue(res1.value()+res2.value());
-                  //   return res; };
+		lexeme exec()
+		{
+			lexeme res1= m_operand1->exec();
+            lexeme res2= m_operand2->exec();
+            lexeme res;
+			//перевод res1.Data() и res2.Data() в числа value1 и value2!
+            res.DataChange(value1 + value2);
+            return res; 
+		}
                   // alt:
                   // { return m_operand1->exec() + m_operand2->exec(); }
-   void print();  // { m_operand1->print(); 
-                  //   printf(" + "); 
-                  //   m_operand2->print(); }
-protected:
-   int     m_type;
-   TNode   m_operand1;
-   TNode   m_operand2;
+		void print()
+		{
+			m_operand1->print(); 
+            printf(" + ");
+            m_operand2->print();
+			printf("\n");
+		}
+	protected:
+		int     m_type;
+		TNode*  m_operand1;
+		TNode*  m_operand2;
 };
 
-class TList : public TNode
+class TList : TNode
 {
-    struct list
-    {
-      TNode sentence;
-      list* next;
-    }
-public:
-    TList()
-    ~TList();
+	public:
+		TList()
+		{
 
-    TValue exec()  {  
-                       list cur = start;
-                       while (cur)
-                       {
-                          cur->sentence->exec();
-                          cur=cur->next;
-                       };
-                       TValue res;
-                       return res;
-                   };
-    void   print() {  
-                       list cur = start;
-                       while (cur)
-                       {
-                          cur->sentence->print();
-                          cur=cur->next;
-                       };
-                   };
-    void  addNode(TNode node);
-
-protected:
-    list* start;
+		}
+		~TList();
+		lexeme exec()  
+		{  
+			list* cur = start;
+            while(cur)
+            {
+				cur->sentence->exec();
+                cur=cur->next;
+            }
+            lexeme res;
+            return res;
+		}
+		void print() 
+		{  
+			list* cur = start;
+			while (cur)
+			{
+				cur->sentence->print();
+				cur=cur->next;
+			}
+		}
+		void  addNode(TNode* node)
+		{
+			
+		}
+	protected:
+		struct list
+		{
+			TNode* sentence;
+			list* next;
+		};
+		list* start;
 };
 
-class TIf : public TNode
+class TIf : TNode
 {
-public:
-      TIf();
-      ~TIf();
+	public:
+		TIf();
+		~TIf();
 
-      TValue exec() { if (condition.exec().value()) return branch_then.exec();
-                      else                          return branch_else.exec();
-                    };
-      void print();
-
-protected:
-    TBinaryOperation condition;
-    TList            branch_then;
-    TList            branch_else;
+		lexeme exec()
+		{ 
+			if(condition.exec().Data)
+				return branch_then.exec();
+			else
+				return branch_else.exec();
+		}
+		void print()
+		{
+			printf("if(");
+			condition.print();
+			printf(")\n{");
+			branch_then.print();
+			printf("\n}\nelse\n{");
+			branch_else.print();
+			printf("}\n");
+		}
+	protected:
+		TBinaryOperation condition;
+		TList branch_then;
+		TList branch_else;
 };
 
 class TWhile
 {
-public:
-      TWhile();
-      ~TWhile();
+	public:
+		TWhile();
+		~TWhile();
 
-      TValue exec() { while (condition.exec().value()) 
-                          body.exec();
-                      return condition.exec();
-                    };
-      void print();
-protected:
-    TBinaryOperation condition;
-    TList            body;
+		lexeme exec() 
+		{
+			while(condition.exec().value()) 
+				body.exec();
+			return condition.exec();
+		}
+		void print();
+	protected:
+		TBinaryOperation condition;
+		TList            body;
 };
