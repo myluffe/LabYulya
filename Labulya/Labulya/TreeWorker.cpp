aa311717a8245b5ad82531facfd739424f259738
Kruglov Dmitry <gryphon-dk@mail.ru>
2016-06-05 21:29:06 +0300

    работа с деревом

--- Labulya/Labulya/TreeWorker.cpp
+++ Labulya/Labulya/TreeWorker.cpp
@@ -1,8 +1,7 @@
-#include "tree.h"
-#include "LexemeWorker.h"
-#include "ErrorReporter.h"
+#include "stdafx.h"
+#include "TreeWorker.h"
 
-static bool DoTree(List* lexes)
+bool TreeWorker::DoTree(List* lexes)
 {
 	int start = 0;
 	TList* prog = (TList*)heap.get_mem(sizeof(TList));
@@ -18,33 +17,38 @@ static bool DoTree(List* lexes)
 	return true;
 }
 
-static TNode* GetTNode(List* lexes, int start, int finish)
+TNode* TreeWorker::GetTNode(List* lexes, int start, int finish)
 {
 	if (start != finish)
 	{
-		int min_prior = ((lexeme*)lexes->get(start))->Priority;
+		int min_prior = ((lexeme*)lexes->get(start))->Priority();
 		int pos_min = start;
 		for (int i = start; i < finish; i++)
 		{
 			lexeme* temp_lexeme = (lexeme*)lexes->get(i);
-			if (temp_lexeme->Priority < min_prior)
+			if (temp_lexeme->Priority() < min_prior)
 			{
-				min_prior = temp_lexeme->Priority;
+				min_prior = temp_lexeme->Priority();
 				pos_min = i;
 			}
 		}
-		if (((lexeme*)lexes->get(pos_min))->Type == BYNARYOPERATION)
-			return new TBinaryOperation(GetTNode(lexes, start, pos_min - 1), GetTNode(lexes, pos_min + 1, finish), (lexeme*)lexes->get(pos_min));
-		if (((lexeme*)lexes->get(pos_min))->Type == UNARYOPERATION)
+		if (((lexeme*)lexes->get(pos_min))->Type() == BYNARYOPERATION)
+			return (TNode*)new TBinaryOperation(GetTNode(lexes, start, pos_min - 1), GetTNode(lexes, pos_min + 1, finish), (lexeme*)lexes->get(pos_min));
+		if (((lexeme*)lexes->get(pos_min))->Type() == UNARYOPERATION)
 		{
 			if (pos_min == finish)
-				return new TUnaryOperation((GetTNode(lexes, start, pos_min - 1), (lexeme*)lexes->get(pos_min), false);
+				return (TNode*)new TUnaryOperation(GetTNode(lexes, start, pos_min - 1), (lexeme*)lexes->get(pos_min), false);
 			if (pos_min == start)
-				return new TUnaryOperation((GetTNode(lexes, pos_min + 1, finish), (lexeme*)lexes->get(pos_min), true);
+				return (TNode*)new TUnaryOperation(GetTNode(lexes, pos_min + 1, finish), (lexeme*)lexes->get(pos_min), true);
 		}
+		return NULL;
 	}
 	else
 	{
-		if (((lexeme*)lexes->get(start))->Type == BYNARYOPERATION)
+		if (strcmp(((lexeme*)lexes->get(start))->Name(), "Variables") == 0)
+			return (TNode*)new TVariable((lexeme*)lexes->get(start));
+		if (strcmp(((lexeme*)lexes->get(start))->Name(), "Numbers") == 0)
+			return (TNode*)new TConst((lexeme*)lexes->get(start));
+		return NULL;
 	}
 }
