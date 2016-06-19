7217ce25b1fb6725250cf57eeb4e52543eb5299d
Gra Kanin <mykotik952@gmail.com>
2016-06-19 18:46:50 +0300

    fix log

--- Labulya/Labulya/Log.cpp
+++ Labulya/Labulya/Log.cpp
@@ -9,11 +9,14 @@ Log::Log(char* FileName)
 
 void Log::Write(char* Out)
 {
-	FILE* f;
-	fopen_s(&f, _logfile, "w");
-	if(f != nullptr)
-		fprintf_s(logfile, "%s\n", Out);
-	fclose(f);
+	if (_logfile != nullptr)
+	{
+		FILE* f;
+		fopen_s(&f, _logfile, "w");
+		if (f != nullptr)
+			fprintf_s(logfile, "%s\n", Out);
+		fclose(f);
+	}
 }
 
 Log::~Log()
